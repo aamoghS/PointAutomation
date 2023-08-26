2ef804ba85d3f90874ec99a250e68170fbf597d8
Aamogh <120258212+aamoghS@users.noreply.github.com>
2023-08-26 11:57:14 -1000

    Update point.py

--- point.py
+++ point.py
@@ -1,34 +1,36 @@
 import pandas as pd
+from datetime import datetime
 
-# First Excel file
+#First Excel file
 file1_data = pd.read_excel('file1.xlsx')
 
-# Second Excel file
+#Second Excel file
 file2_data = pd.read_excel('file2.xlsx')
 
-# group the files 
+# Group names
 points_dict = file1_data.groupby(['First Name', 'Last Name'])['Points'].sum().to_dict()
 
-# Merge mile and add points 
+# merage the data
 merged_data = []
 for index, row in file2_data.iterrows():
     first_name = row['First Name']
     last_name = row['Last Name']
     points = points_dict.get((first_name, last_name), 0) + 1
-    merged_data.append({'First Name': first_name, 'Last Name': last_name, 'Points': points})
+    prev_points = points_dict.get((first_name, last_name), 0)  # Previous points
+    points_difference = points - prev_points  # Calculate points difference
+    date_added = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Get current date and time
+    merged_data.append({
+        'First Name': first_name,
+        'Last Name': last_name,
+        'Points': points,
+        'Points Added': points_difference,
+        'Date Added': date_added
+    })
 
-# merged to a frame 
+# to df 
 merged_df = pd.DataFrame(merged_data)
 
-# additional
-additional_columns = list(file2_data.columns)
-additional_columns.remove('First Name')
-additional_columns.remove('Last Name')
-
-for col in additional_columns:
-    merged_df[col] = file2_data[col]
-
-# saves
+# save as excel
 merged_df.to_excel('merged_file.xlsx', index=False)
 
-print("Updated.")
+print("Excel files merged and updated.")
